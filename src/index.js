06d0794cb1d62acdc8d944bfd241ff68c1c73ece
Shivansh <shivanshraghav7@gmail.com>
2023-10-17 16:57:38 +0530

    tinker

--- src/index.js
+++ src/index.js
@@ -2,8 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { ConfigProvider } from 'antd';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App />
+  <ConfigProvider>
+    <App theme={{ hashed: false }}></App>
+  </ConfigProvider>
 );
 
